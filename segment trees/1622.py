da496ba0b78cb42db0bf032542f7435c83a59b1a
unknown <dimitry008@gmail.com>
2022-01-16 21:09:40 +0300

    CF 1626

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 I = lambda: [int(x) for x in input().split()]
-import io, os, sys
-#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 
 ################################################
 from types import GeneratorType
@@ -24,58 +22,42 @@ def bootstrap(f, stack=[]):
     return wrappedfunc
 ################################################
 
-class Counter:
-    def __init__(self, c):
-        self.ans = c
-        self.c_t = defaultdict(lambda: 0)
-        self.mx = self.c_t[c] = 1
-        self.t_c = defaultdict(set)
-        self.t_c[1] = {c}
-
-    def merge(self, other):
-        for c, t in other.c_t.items():
-            self.add(c, t)
-
-    def add(self, c, t):
-        old_t = self.c_t[c]
-        self.c_t[c] = new_t = old_t + t
-        self.t_c[old_t].discard(c)
-        self.t_c[new_t].add(c)
-        if new_t == self.mx:
-            self.ans += c
-        elif new_t > self.mx:
-            self.mx = new_t
-            self.ans = c
-
-    def __len__(self):
-        return len(self.c_t)
-
-    def __repr__(self):
-        return str(self.ans)
 
 @bootstrap
-def dfs(par, node):
-    c = color[node]
-    ans_n = Counter(c)
+def dfs1(u, p):
+    cnt[u] = c[u]
+    for v in G[u]:
+        if v == p: continue
+        yield dfs1(v, u)
+        cnt[u] += cnt[v]
+        if ans[v] and cnt[v] >= 2: ans[u] = 1
+    yield
 
-    for child in G[node]:
-        if child == par: continue
-        ans_c = yield dfs(node, child)
-        if len(ans_n) < len(ans_c):
-            ans_n, ans_c = ans_c, ans_n
-        ans_n.merge(ans_c)
-    out[node] = ans_n.ans
-    yield ans_n
+
+@bootstrap
+def dfs2(u, p):
+    for v in G[u]:
+        if v == p: continue
+        if ans[u] and tot - cnt[v] >= 2: ans[v] = 1
+        yield dfs2(v, u)
+    yield
 
 n = int(input())
-color = I()
+c = I()
+cnt, ans = [0]*n, [0]*n
 G = defaultdict(list)
-out = [0]*n
-for _ in range(n-1):
-    a, b = I()
-    G[a-1] += [b-1]
-    G[b-1] += [a-1]
+tot = sum(c)
+for i in range(n-1):
+    u, v = I()
+    G[u-1] += [v-1]
+    G[v-1] += [u-1]
 
-dfs(-1, 0)
-sys.stdout.write(" ".join(map(str, out)) + "\n")
+for i in range(n):
+    if not c[i]: continue
+    ans[i] = 1
+    for v in G[i]: ans[v] = 1
 
+dfs1(0, -1)
+dfs2(0, -1)
+
+print(" ".join(map(str, ans)) + "\n")
