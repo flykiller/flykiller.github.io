aa7bbde569c9b011c113f5149ee97973430c8018
unknown <dimitry008@gmail.com>
2022-01-17 19:27:48 +0300

    CF 1626F

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,63 +1,21 @@
-from collections import defaultdict, Counter
+M, N, res = 998244353, 720720, 0
 I = lambda: [int(x) for x in input().split()]
+p = [0] * N
 
-################################################
-from types import GeneratorType
-def bootstrap(f, stack=[]):
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
-                        break
-                    to = stack[-1].send(to)
-            return to
-    return wrappedfunc
-################################################
+n, a, x, y, k, U = I()
+for i in range(1, n + 1):
+    p[a % N] += 1
+    res += a // N * N
+    a = (a * x + y) % U
 
+res = (res * pow(n, k - 1, M) * k) % M
+for i in range(1, k + 1):
+    tot = sum(p[j] * j for j in range(N)) % M
+    for j in range(N):
+        tmp = p[j]
+        p[j] *= (n - 1)
+        p[j] %= M
+        p[j - j % i] += tmp
+    res += (tot * pow(n, k - i)) % M
 
-@bootstrap
-def dfs1(u, p):
-    cnt[u] = c[u]
-    for v in G[u]:
-        if v == p: continue
-        yield dfs1(v, u)
-        cnt[u] += cnt[v]
-        if ans[v] and cnt[v] >= 2: ans[u] = 1
-    yield
-
-
-@bootstrap
-def dfs2(u, p):
-    for v in G[u]:
-        if v == p: continue
-        if ans[u] and tot - cnt[v] >= 2: ans[v] = 1
-        yield dfs2(v, u)
-    yield
-
-n = int(input())
-c = I()
-cnt, ans = [0]*n, [0]*n
-G = defaultdict(list)
-tot = sum(c)
-for i in range(n-1):
-    u, v = I()
-    G[u-1] += [v-1]
-    G[v-1] += [u-1]
-
-for i in range(n):
-    if not c[i]: continue
-    ans[i] = 1
-    for v in G[i]: ans[v] = 1
-
-dfs1(0, -1)
-dfs2(0, -1)
-
-print(" ".join(map(str, ans)) + "\n")
+print(res % M)
