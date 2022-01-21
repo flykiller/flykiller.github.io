4d81a6a19ccd6f5822ea99608d216acf4e5acbb1
unknown <dimitry008@gmail.com>
2022-01-21 11:42:59 +0300

    CF 0616

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,56 +1,368 @@
+class SortedList:
+    def __init__(self, iterable=[], _load=200):
+        """Initialize sorted list instance."""
+        values = sorted(iterable)
+        self._len = _len = len(values)
+        self._load = _load
+        self._lists = _lists = [values[i:i + _load] for i in range(0, _len, _load)]
+        self._list_lens = [len(_list) for _list in _lists]
+        self._mins = [_list[0] for _list in _lists]
+        self._fen_tree = []
+        self._rebuild = True
+
+    def _fen_build(self):
+        """Build a fenwick tree instance."""
+        self._fen_tree[:] = self._list_lens
+        _fen_tree = self._fen_tree
+        for i in range(len(_fen_tree)):
+            if i | i + 1 < len(_fen_tree):
+                _fen_tree[i | i + 1] += _fen_tree[i]
+        self._rebuild = False
+
+    def _fen_update(self, index, value):
+        """Update `fen_tree[index] += value`."""
+        if not self._rebuild:
+            _fen_tree = self._fen_tree
+            while index < len(_fen_tree):
+                _fen_tree[index] += value
+                index |= index + 1
+
+    def _fen_query(self, end):
+        """Return `sum(_fen_tree[:end])`."""
+        if self._rebuild:
+            self._fen_build()
+
+        _fen_tree = self._fen_tree
+        x = 0
+        while end:
+            x += _fen_tree[end - 1]
+            end &= end - 1
+        return x
+
+    def _fen_findkth(self, k):
+        """Return a pair of (the largest `idx` such that `sum(_fen_tree[:idx]) <= k`, `k - sum(_fen_tree[:idx])`)."""
+        _list_lens = self._list_lens
+        if k < _list_lens[0]:
+            return 0, k
+        if k >= self._len - _list_lens[-1]:
+            return len(_list_lens) - 1, k + _list_lens[-1] - self._len
+        if self._rebuild:
+            self._fen_build()
+
+        _fen_tree = self._fen_tree
+        idx = -1
+        for d in reversed(range(len(_fen_tree).bit_length())):
+            right_idx = idx + (1 << d)
+            if right_idx < len(_fen_tree) and k >= _fen_tree[right_idx]:
+                idx = right_idx
+                k -= _fen_tree[idx]
+        return idx + 1, k
+
+    def _delete(self, pos, idx):
+        """Delete value at the given `(pos, idx)`."""
+        _lists = self._lists
+        _mins = self._mins
+        _list_lens = self._list_lens
+
+        self._len -= 1
+        self._fen_update(pos, -1)
+        del _lists[pos][idx]
+        _list_lens[pos] -= 1
+
+        if _list_lens[pos]:
+            _mins[pos] = _lists[pos][0]
+        else:
+            del _lists[pos]
+            del _list_lens[pos]
+            del _mins[pos]
+            self._rebuild = True
+
+    def _loc_left(self, value):
+        """Return an index pair that corresponds to the first position of `value` in the sorted list."""
+        if not self._len:
+            return 0, 0
+
+        _lists = self._lists
+        _mins = self._mins
+
+        lo, pos = -1, len(_lists) - 1
+        while lo + 1 < pos:
+            mi = (lo + pos) >> 1
+            if value <= _mins[mi]:
+                pos = mi
+            else:
+                lo = mi
+
+        if pos and value <= _lists[pos - 1][-1]:
+            pos -= 1
+
+        _list = _lists[pos]
+        lo, idx = -1, len(_list)
+        while lo + 1 < idx:
+            mi = (lo + idx) >> 1
+            if value <= _list[mi]:
+                idx = mi
+            else:
+                lo = mi
+
+        return pos, idx
+
+    def _loc_right(self, value):
+        """Return an index pair that corresponds to the last position of `value` in the sorted list."""
+        if not self._len:
+            return 0, 0
+
+        _lists = self._lists
+        _mins = self._mins
+
+        pos, hi = 0, len(_lists)
+        while pos + 1 < hi:
+            mi = (pos + hi) >> 1
+            if value < _mins[mi]:
+                hi = mi
+            else:
+                pos = mi
+
+        _list = _lists[pos]
+        lo, idx = -1, len(_list)
+        while lo + 1 < idx:
+            mi = (lo + idx) >> 1
+            if value < _list[mi]:
+                idx = mi
+            else:
+                lo = mi
+
+        return pos, idx
+
+    def add(self, value):
+        """Add `value` to sorted list."""
+        _load = self._load
+        _lists = self._lists
+        _mins = self._mins
+        _list_lens = self._list_lens
+
+        self._len += 1
+        if _lists:
+            pos, idx = self._loc_right(value)
+            self._fen_update(pos, 1)
+            _list = _lists[pos]
+            _list.insert(idx, value)
+            _list_lens[pos] += 1
+            _mins[pos] = _list[0]
+            if _load + _load < len(_list):
+                _lists.insert(pos + 1, _list[_load:])
+                _list_lens.insert(pos + 1, len(_list) - _load)
+                _mins.insert(pos + 1, _list[_load])
+                _list_lens[pos] = _load
+                del _list[_load:]
+                self._rebuild = True
+        else:
+            _lists.append([value])
+            _mins.append(value)
+            _list_lens.append(1)
+            self._rebuild = True
+
+    def discard(self, value):
+        """Remove `value` from sorted list if it is a member."""
+        _lists = self._lists
+        if _lists:
+            pos, idx = self._loc_right(value)
+            if idx and _lists[pos][idx - 1] == value:
+                self._delete(pos, idx - 1)
+
+    def remove(self, value):
+        """Remove `value` from sorted list; `value` must be a member."""
+        _len = self._len
+        self.discard(value)
+        if _len == self._len:
+            raise ValueError('{0!r} not in list'.format(value))
+
+    def pop(self, index=-1):
+        """Remove and return value at `index` in sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        value = self._lists[pos][idx]
+        self._delete(pos, idx)
+        return value
+
+    def bisect_left(self, value):
+        """Return the first index to insert `value` in the sorted list."""
+        pos, idx = self._loc_left(value)
+        return self._fen_query(pos) + idx
+
+    def bisect_right(self, value):
+        """Return the last index to insert `value` in the sorted list."""
+        pos, idx = self._loc_right(value)
+        return self._fen_query(pos) + idx
+
+    def count(self, value):
+        """Return number of occurrences of `value` in the sorted list."""
+        return self.bisect_right(value) - self.bisect_left(value)
+
+    def __len__(self):
+        """Return the size of the sorted list."""
+        return self._len
+
+    def __getitem__(self, index):
+        """Lookup value at `index` in sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        return self._lists[pos][idx]
+
+    def __delitem__(self, index):
+        """Remove value at `index` from sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        self._delete(pos, idx)
+
+    def __contains__(self, value):
+        """Return true if `value` is an element of the sorted list."""
+        _lists = self._lists
+        if _lists:
+            pos, idx = self._loc_left(value)
+            return idx < len(_lists[pos]) and _lists[pos][idx] == value
+        return False
+
+    def __iter__(self):
+        """Return an iterator over the sorted list."""
+        return (value for _list in self._lists for value in _list)
+
+    def __reversed__(self):
+        """Return a reverse iterator over the sorted list."""
+        return (value for _list in reversed(self._lists) for value in reversed(_list))
+
+    def __repr__(self):
+        """Return string representation of sorted list."""
+        return 'SortedList({0})'.format(list(self))
+
+
+##############################################
+
+def suffixArray(A):
+    n = len(A)
+    buckets = [0] * (max(A) + 2)
+    for a in A:
+        buckets[a + 1] += 1
+    for b in range(1, len(buckets)):
+        buckets[b] += buckets[b - 1]
+    isL = [1] * n
+    for i in reversed(range(n - 1)):
+        isL[i] = +(A[i] > A[i + 1]) if A[i] != A[i + 1] else isL[i + 1]
+
+    def induced_sort(LMS):
+        SA = [-1] * (n)
+        SA.append(n)
+        endpoint = buckets[1:]
+        for j in reversed(LMS):
+            endpoint[A[j]] -= 1
+            SA[endpoint[A[j]]] = j
+        startpoint = buckets[:-1]
+        for i in range(-1, n):
+            j = SA[i] - 1
+            if j >= 0 and isL[j]:
+                SA[startpoint[A[j]]] = j
+                startpoint[A[j]] += 1
+        SA.pop()
+        endpoint = buckets[1:]
+        for i in reversed(range(n)):
+            j = SA[i] - 1
+            if j >= 0 and not isL[j]:
+                endpoint[A[j]] -= 1
+                SA[endpoint[A[j]]] = j
+        return SA
+
+    isLMS = [+(i and isL[i - 1] and not isL[i]) for i in range(n)]
+    isLMS.append(1)
+    LMS = [i for i in range(n) if isLMS[i]]
+    if len(LMS) > 1:
+        SA = induced_sort(LMS)
+        LMS2 = [i for i in SA if isLMS[i]]
+        prev = -1
+        j = 0
+        for i in LMS2:
+            i1 = prev
+            i2 = i
+            while prev >= 0 and A[i1] == A[i2]:
+                i1 += 1
+                i2 += 1
+                if isLMS[i1] or isLMS[i2]:
+                    j -= isLMS[i1] and isLMS[i2]
+                    break
+            j += 1
+            prev = i
+            SA[i] = j
+        LMS = [LMS[i] for i in suffixArray([SA[i] for i in LMS])]
+    return induced_sort(LMS)
+
+def lcp(A, SA):
+    n = len(A)
+    rank = [0] * n
+    for i in range(n):
+        rank[SA[i]] = i
+    LCP = [0] * (n - 1)
+    k = 0
+    for i in range(n):
+        SAind = rank[i]
+        if SAind == n - 1:
+            continue
+        j = SA[SAind + 1]
+        while i + k < n and A[i + k] == A[j + k]:
+            k += 1
+        LCP[SAind] = k
+        k -= k > 0
+    return LCP, rank
+
+##############################################
+
 from collections import defaultdict
 
-def perm_loops(x, n): # from permutation create loops, permutation eg [1, 0, 4, 2, 3]
-    V, ans = set(), []
-    for i in range(n):
-        if i in V: continue
-        q = i
-        ans += [[q]]
-        V.add(i)
-        while x[q] != i:
-            ans[-1] += [x[q]]
-            q = x[q]
-            V.add(q)
-    return ans
-
-def loops_perm(loops, n):  # from loops create permutation
-    ans = [0] * n
-    for loop in loops:
-        for x, y in zip(loop, loop[1:] + loop[:1]):
-            ans[x] = y
-    return ans
-
 n = int(input())
-arr = [int(i) - 1 for i in input().split()]
-d_loops = defaultdict(list)
+arr, A, B = [], [], []
+for _ in range(n):
+    arr += [input().rstrip()]
 
-loops = perm_loops(arr, n)
-for loop in loops:
-    d_loops[len(loop)] += [tuple(loop)]
+CC = [int(i) for i in input().split()]
+for i in range(n):
+    A += [ord(j) - 96 for j in arr[i]]
+    B += [CC[i]] * len(arr[i])
+    A += [i + 27]
+    B += [0]  # it should not matter?
 
-def solve(d_loops):
-    ans = []
-    for d in d_loops:
-        if d % 2 == 1:
-            for loop in d_loops[d]:
-                tmp = [0]*d
-                tmp[::2] = loop[:d//2+1]
-                tmp[1::2] = loop[d//2+1:]
-                ans += [tmp]
-        else:
-            if len(d_loops[d]) % 2 == 1:
-                return []
-            else:
-                for i in range(len(d_loops[d])//2):
-                    tmp = [0]*(2*d)
-                    tmp[::2] = d_loops[d][2*i]
-                    tmp[1::2] = d_loops[d][2*i+1]
-                    ans += [tmp]
-    return ans
 
-ans = solve(d_loops)
-if ans == []:
-    print(-1)
-else:
-    out = loops_perm(ans, n)
-    print(" ".join(str(i+1) for i in out))
+sa = suffixArray(A)
+cnts = [B[i] for i in sa]
+LCP, rank = lcp(A, sa)
+N = len(sa)
+f1 = list(range(N))
+f2 = list(range(N))
+
+print(LCP)
+
+d_lcp = defaultdict(list)
+for i, x in enumerate(LCP[:-1]):
+    d_lcp[x] += [i]
+
+keys_lcp = sorted(d_lcp.keys())[::-1]
+ans = 0
+
+for i in range(N):
+    if i == 0 or A[i-1] >= 27:
+        q1 = LCP[rank[i] - 1]
+        q2 = LCP[rank[i]]
+        idx = 0 if i == 0 else A[i-1] - 26
+        if q1 != len(arr[idx]) and q2 != len(arr[idx]):
+            ans = max(ans, len(arr[idx]) * CC[idx])
+
+costs = SortedList()
+
+for key in keys_lcp:
+    for u in d_lcp[key]:
+        v1 = f1[u + 1]
+        u1 = f2[u]
+        cu, cv = cnts[u1], cnts[u + 1]
+        if u1 != u: costs.remove((cu, u1, u))
+        if u + 1 != v1: costs.remove((cv, u + 1, v1))
+        cnts[u1] += cv
+        costs.add((cu + cv, u1, v1))
+        f1[u1] = v1
+        f2[v1] = u1
+    ans = max(ans, costs[-1][0] * key)
+
+print(ans)
