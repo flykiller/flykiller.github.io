a1f16ae48bcb930184dac479d72265471a067e11
unknown <dimitry008@gmail.com>
2022-02-01 22:55:33 +0300

    CF1633E fixed

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,7 +1,5 @@
-from bisect import bisect_left, bisect
-import io, os, sys
-from collections import Counter
-#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+from bisect import bisect_left
+from collections import defaultdict
 I = lambda: [int(x) for x in input().split()]
 
 class DSU:
@@ -14,57 +12,57 @@ class DSU:
         return self.p[x]
 
     def union(self, x, y):
-        xr = self.find(x)
-        yr = self.find(y)
-        self.p[xr] = yr
-
-def kruskal(n, edges, XX):
-    dsu, ans, sgn = DSU(n), 0, 0
-    for x, y, w in edges:
-        s =  -1 + 2*int(XX <= w)
-        if dsu.find(x) == dsu.find(y): continue
-        dsu.union(x, y)
-        ans += w*s
-        sgn += s
-    return ans, sgn
+        self.p[self.find(x)] = self.find(y)
 
 
 edges = []
 n, m = I()
 for _ in range(m):
     x, y, w = I()
-    edges += [(x-1, y-1, 2*w)]
-
-E = sorted([w for _, _, w in edges])
-
-w, ans, out = [], [], 0
-
-for i in range(m):
-    for j in range(i, m):
-        w += [(E[i]+E[j])//2]
-
-w = sorted(set(w))
-ans = [(-1, -1)] * len(w)
-
-#for i in range(len(w)):
-#    edges = sorted(edges, key=lambda t: abs(w[i] - t[2]))
-#    ans[i] = kruskal(n, edges, w[i])
+    edges += [(x - 1, y - 1, w)]
 
 p, k, a, b, c = I()
 Q = I()
 for _ in range(k - p):
     Q += [(Q[-1] * a + b) % c]
 
-cc = Counter(Q)
-Q = [ee for ee in cc if (cc[ee] % 2 == 1)]
-Q.sort()
+def kruskal(x):
+    dsu, ans, W, sgn = DSU(n), [], 0, 0
+    E = sorted(edges, key=lambda q: abs(x - q[2]))
+    for u, v, w in E:
+        if dsu.find(u) == dsu.find(v): continue
+        s = -1 + 2 * int(x <= w)
+        dsu.union(u, v)
+        ans += [w]
+        W += w * s
+        sgn += s
+    return sorted(ans), W, sgn
 
-t = 0
+points = defaultdict(tuple)
+at, maxval = 0, 10**8
+
+while at <= maxval:
+    cur_weights, wei, sgn = kruskal(at)
+    lo, hi = at, maxval
+    while lo < hi:
+        mid = (lo + hi + 1) // 2
+        cur_weights2, wei2, sgn2 = kruskal(mid)
+        if cur_weights2 == cur_weights:
+            lo = mid
+        else:
+            hi = mid - 1
+
+    points[lo] = kruskal(lo)
+    at = lo + 1
+
+for _, _, w in edges:
+    points[w] = kruskal(w)
+
+w, out = sorted(points), 0
+
 for x in Q:
-    while t < len(w) - 1 and w[t] < 2*x: t += 1
-    if ans[t] == (-1, -1):
-        edges = sorted(edges, key=lambda q: abs(w[t] - q[2]))
-        ans[t] = kruskal(n, edges, w[t])
-    out ^= (ans[t][0]//2 - x*ans[t][1])
+    idx = bisect_left(w, x)
+    if idx >= len(w): idx -= 1
+    out ^= (points[w[idx]][1] - x*points[w[idx]][2])
 
 print(out)
