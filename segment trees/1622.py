332fde0fa82601c3ccebc91b94b39b6f8c34e85b
unknown <dimitry008@gmail.com>
2022-02-07 00:05:02 +0300

    CF 1634

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,68 +1,38 @@
-from bisect import bisect_left
-from collections import defaultdict
 I = lambda: [int(x) for x in input().split()]
+import sys
 
-class DSU:
-    def __init__(self, N):
-        self.p = list(range(N))
+def q():
+    val = int(input())
+    return val
 
-    def find(self, x):
-        if self.p[x] != x:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
+def p(v):
+    print(v)
+    sys.stdout.flush()
 
-    def union(self, x, y):
-        self.p[self.find(x)] = self.find(y)
+def eliminate(i1, i2, i3, i4):
+    s1, s2, s3, s4 = str(i1), str(i2), str(i3), str(i4)
+    V = []
+    for i, x, y, z in (i1, s2, s3, s4), (i2, s1, s3, s4), (i3, s1, s2, s4), (i4, s1, s2, s3):
+        p("? " + x + " " + y + " " + z)
+        V += [(q(), i)]
+    V = sorted(V)
+    return V[-1][1], V[-2][1]
 
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    indexes = list(range(1, n+1))
+    while len(indexes) > 5:
+        a, b = eliminate(*indexes[-4:])
+        indexes.remove(a)
+        indexes.remove(b)
 
-edges = []
-n, m = I()
-for _ in range(m):
-    x, y, w = I()
-    edges += [(x - 1, y - 1, w)]
+    if len(indexes) == 5:
+        a, _ = eliminate(*indexes[-4:])
+        indexes.remove(a)
 
-p, k, a, b, c = I()
-Q = I()
-for _ in range(k - p):
-    Q += [(Q[-1] * a + b) % c]
+    a, b = eliminate(*indexes[-4:])
+    indexes.remove(a)
+    indexes.remove(b)
 
-def kruskal(x):
-    dsu, ans, W, sgn = DSU(n), [], 0, 0
-    E = sorted(edges, key=lambda q: abs(x - q[2]))
-    for u, v, w in E:
-        if dsu.find(u) == dsu.find(v): continue
-        s = -1 + 2 * int(x <= w)
-        dsu.union(u, v)
-        ans += [w]
-        W += w * s
-        sgn += s
-    return sorted(ans), W, sgn
-
-points = defaultdict(tuple)
-at, maxval = 0, 10**8
-
-while at <= maxval:
-    cur_weights, wei, sgn = kruskal(at)
-    lo, hi = at, maxval
-    while lo < hi:
-        mid = (lo + hi + 1) // 2
-        cur_weights2, wei2, sgn2 = kruskal(mid)
-        if cur_weights2 == cur_weights:
-            lo = mid
-        else:
-            hi = mid - 1
-
-    points[lo] = kruskal(lo)
-    at = lo + 1
-
-for _, _, w in edges:
-    points[w] = kruskal(w)
-
-w, out = sorted(points), 0
-
-for x in Q:
-    idx = bisect_left(w, x)
-    if idx >= len(w): idx -= 1
-    out ^= (points[w[idx]][1] - x*points[w[idx]][2])
-
-print(out)
+    p("! " + " ".join(str(x) for x in indexes) + "\n")
