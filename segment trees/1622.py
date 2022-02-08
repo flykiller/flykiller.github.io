f0712c0d33321386f671d47548c35b80b7de403b
unknown <dimitry008@gmail.com>
2022-02-08 13:15:45 +0300

    BS -0187

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,38 +1,32 @@
+import io, os, sys
+#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 I = lambda: [int(x) for x in input().split()]
-import sys
 
-def q():
-    val = int(input())
-    return val
+def update(i, d, cnt):
+    cnt -= (unfib[i] != 0)
+    unfib[i] = (unfib[i] + d) % MOD
+    cnt += (unfib[i] != 0)
+    return cnt
 
-def p(v):
-    print(v)
-    sys.stdout.flush()
+n, q, MOD = I()
+a = I()
+b = I()
+diff = [0, 0] + [(y - x) % MOD for x, y in zip(a, b)]
+fib = [1] * n
+for i in range(2, n):
+    fib[i] = (fib[i - 1] + fib[i - 2]) % MOD
 
-def eliminate(i1, i2, i3, i4):
-    s1, s2, s3, s4 = str(i1), str(i2), str(i3), str(i4)
-    V = []
-    for i, x, y, z in (i1, s2, s3, s4), (i2, s1, s3, s4), (i3, s1, s2, s4), (i4, s1, s2, s3):
-        p("? " + x + " " + y + " " + z)
-        V += [(q(), i)]
-    V = sorted(V)
-    return V[-1][1], V[-2][1]
+unfib = [(diff[i+2] - diff[i + 1] - diff[i]) % MOD for i in range(n)]
 
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    indexes = list(range(1, n+1))
-    while len(indexes) > 5:
-        a, b = eliminate(*indexes[-4:])
-        indexes.remove(a)
-        indexes.remove(b)
+cnt = sum(x != 0 for x in unfib)
+result = []
+for _ in range(q):
+    c, l, r = input().split()
+    l, r = int(l) - 1, int(r) - 1
+    sign = 1 if c == 'A' else -1
+    cnt = update(l, -sign, cnt)
+    if r + 1 < n: cnt = update(r + 1, sign * fib[r - l + 1], cnt)
+    if r + 2 < n: cnt = update(r + 2, sign * fib[r - l], cnt)
+    result += ["YES" if cnt == 0 else "NO"]
 
-    if len(indexes) == 5:
-        a, _ = eliminate(*indexes[-4:])
-        indexes.remove(a)
-
-    a, b = eliminate(*indexes[-4:])
-    indexes.remove(a)
-    indexes.remove(b)
-
-    p("! " + " ".join(str(x) for x in indexes) + "\n")
+sys.stdout.write("\n".join(result))
