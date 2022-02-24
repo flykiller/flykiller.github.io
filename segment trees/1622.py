444af8a573b5072934034be77680f4094506b9e1
unknown <dimitry008@gmail.com>
2022-02-24 20:25:59 +0300

    CF 1644E

--- segment trees/1622.py
+++ segment trees/1622.py
@@ -1,107 +1,64 @@
-class SegmentTree:
-    def __init__(self, n):
-        self.size = 1
-        while self.size < n:
-            self.size *= 2
-        self.T = [0] * (2 * self.size - 1)   # to multiply
-        self.L = [0] * (2 * self.size - 1)   # current sum
-        self.ZERO = 0  # neutral element, for min it is +inf
-        self.NO_OPERATION = -float("inf")  # this is for our propagate to understand that we do not need it
-
-    def op_modify(self, a, b, len_):
-        if b == self.NO_OPERATION:
-            return a
-        return b * len_
-
-    def op_sum(self, a, b):
-        return max(a, b)
-
-    def propagate(self, x, lx, rx):
-        if self.L[x] == self.NO_OPERATION or rx - lx == 1:
-            return
-        mx = (lx + rx)//2
-        self.L[2 * x + 1] = self.op_modify(self.L[2 * x + 1], self.L[x], 1)
-        self.T[2 * x + 1] = self.op_modify(self.T[2 * x + 1], self.L[x], mx - lx)  # in fact we do not use len
-        self.L[2 * x + 2] = self.op_modify(self.L[2 * x + 2], self.L[x], 1)
-        self.T[2 * x + 2] = self.op_modify(self.T[2 * x + 2], self.L[x], rx - mx)
-        self.L[x] = self.NO_OPERATION
-
-    def _update(self, l, r, v, x, lx, rx):
-        self.propagate(x, lx, rx)
-        if l >= rx or lx >= r:
-            return
-        if lx >= l and rx <= r:
-            self.L[x] = self.op_modify(self.L[x], v, 1)        # why 1?
-            self.T[x] = self.op_modify(self.T[x], v, rx - lx)
-            return
-        mx = (lx + rx)//2
-        self._update(l, r, v, 2*x+1, lx, mx)
-        self._update(l, r, v, 2*x+2, mx, rx)
-        self.T[x] = self.op_sum(self.T[2*x+1], self.T[2*x+2])
-
-    def update(self, l, r, v):
-        return self._update(l, r, v, 0, 0, self.size)
-
-    def _query(self, l, r, x, lx, rx):
-        self.propagate(x, lx, rx)
-        if l >= rx or lx >= r:
-            return self.ZERO
-        if lx >= l and rx <= r:
-            return self.T[x]
-        mx = (lx + rx) // 2
-        m1 = self._query(l, r, 2 * x + 1, lx, mx)
-        m2 = self._query(l, r, 2 * x + 2, mx, rx)
-        return self.op_sum(m1, m2)
-
-    def query(self, l, r):
-        return self._query(l, r, 0, 0, self.size)
-
-    def first_above_(self, v, l, x, lx, rx):
-        self.propagate(x, lx, rx)
-        if self.T[x] < v or rx <= l:
-            return -1
-        if rx - lx == 1:
-            return lx
-        mx = (lx + rx) // 2
-        res = self.first_above_(v, l, 2 * x + 1, lx, mx)
-        if res == -1:
-            res = self.first_above_(v, l, 2 * x + 2, mx, rx)
-        return res
-
-    def first_above(self, v, l):
-        return self.first_above_(v, l, 0, 0, self.size)
-
 I = lambda: [int(i) for i in input().split()]
+import io, os, sys
+#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+
+class DSU:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.rnk = [0] * n
+        self.lft = list(range(n))
+        self.rgh = list(range(n))
+        self.lst = [n - 1] * n
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y: return False
+        if self.rnk[x] > self.rnk[y]:
+            x, y = y, x
+        self.p[x] = self.p[y]
+        if self.rnk[x] == self.rnk[y]:
+            self.rnk[y] += 1
+        self.lft[y] = min(self.lft[x], self.lft[y])
+        self.rgh[y] = max(self.rgh[x], self.rgh[y])
+        self.lst[y] = min(self.lst[x], self.lst[y])
+        return True
+
+    def find_lft(self, x):
+        return self.lft[dsu.find(x)]
+
+    def find_rgh(self, x):
+        return self.rgh[dsu.find(x)]
+
+    def find_lst(self, x):
+        return self.lst[dsu.find(x)]
 
 n, q = I()
-STree = SegmentTree(n)
-STree.update(0, n, 1)
-
-
-ill = set()
-
+dsu, ans = DSU(n + 2), []
 for _ in range(q):
-    arr = I()
-    if arr[0] == 0 and arr[3] == 0:
-        STree.update(arr[1] - 1, arr[2], 0)
-    elif arr[0] == 0 and arr[3] == 1:
-        sm = STree.query(arr[1] - 1, arr[2])
-        l, r = arr[1] - 1, arr[2] + 1
-        while l + 1 < r:
-            m = (l + r)//2
-            if STree.query(l, r) == 1:
-                r = m
-            else:
-                l = m
-        ill.add(l + 1)
-    else:
-        tt = arr[1] - 1
-        if tt in ill:
-            print("YES")
-        elif STree.query(tt, tt + 1) == 0:
-            print("NO")
+    args = I()
+    if args[0] == 0:
+        l, r, x = args[1:]
+        idx_l = dsu.find(l - 1)
+        idx_r = dsu.find(r)
+        if x:
+            dsu.lst[idx_l] = min(dsu.lst[idx_l], r)
         else:
-            print("N/A")
+            beg, end = dsu.rgh[idx_l], dsu.lft[idx_r]
+            while beg < end:
+                dsu.union(beg, beg + 1)
+                beg = dsu.find_rgh(beg)
+    else:
+        j = args[1]
+        if dsu.find(j - 1) == dsu.find(j):
+            ans += ["NO"]
+        elif dsu.find_lft(dsu.find_lst(j - 1)) == j:
+            ans += ["YES"]
+        else:
+            ans += ["N/A"]
 
-    #print("ILL", ill)
-
+print("\n".join(ans))
